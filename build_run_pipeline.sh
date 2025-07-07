7a8f6e510f7c620b4c4f372095efe4e380f2fe38
dkedinger <daniel@meshbr.com>
2025-07-07 00:02:42 -0500

    Update build_run_pipeline.sh

--- build_run_pipeline.sh
+++ build_run_pipeline.sh
@@ -3,6 +3,13 @@ set -euo pipefail
 
 cd /root/library
 
+# Remove the previous image
+docker stop mesh-library-app
+docker rm mesh-library-app
+
+# Rebuild the Docker image
+docker build -t mesh-library-app .
+
 docker run -d -p 3000:3000 \
   --name mesh-library-app \
   --env-file .env \
