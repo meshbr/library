ddd006404d81e6056d5ba09ee379cfb350e31c39
dkedinger <daniel@meshbr.com>
2025-07-07 08:08:36 -0500

    Update run_mesh_library_app.sh

--- run_mesh_library_app.sh
+++ run_mesh_library_app.sh
@@ -3,6 +3,13 @@ set -euo pipefail
 
 cd /root/library
 
+# Check if the container already exists and remove it
+if [ "$(docker ps -aq -f name=mesh-library-app)" ]; then
+    docker rm -f mesh-library-app
+fi
+
+# Start the Docker container
+echo "Starting the mesh-library-app container..."
 docker run -d -p 3000:3000 \
   --name mesh-library-app \
   --env-file .env \
